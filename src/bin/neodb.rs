0cbc0174f0405110521f2716aeec4f9fdb8c2f16
GitHub <=>
2022-10-19 11:49:46 +0800

    update

--- src/bin/neodb.rs
+++ src/bin/neodb.rs
@@ -1,5 +1,9 @@
 use bincode::config::standard;
-use datura::{config::CONFIG, extract::Book, ivec_to_u32, u32_to_ivec, CLIENT};
+use datura::{
+    config::CONFIG,
+    extract::{Book, Movie},
+    ivec_to_u32, u32_to_ivec, CLIENT,
+};
 use reqwest::StatusCode;
 use std::{path::PathBuf, sync::Arc, time::Duration};
 use tokio::sync::Semaphore;
@@ -18,6 +22,160 @@ async fn main() {
     let db = config.open().unwrap();
     info!(%db_url);
 
+    //movies
+    let movies_tree = db.open_tree("movies").unwrap();
+    let db_max_id = if let Some((k, _)) = movies_tree.last().unwrap() {
+        ivec_to_u32(&k)
+    } else {
+        0
+    };
+    info!(%db_max_id);
+
+    let min_id = CONFIG.movie_min_id.unwrap_or(db_max_id);
+    let max_id = CONFIG.movie_max_id;
+    let site = "https://neodb.social/movies";
+
+    info!(%min_id);
+    info!(%max_id);
+
+    let movie_404_tree = db.open_tree("movie_404").unwrap();
+    let mut ids = vec![];
+    for id in min_id..=max_id {
+        if !movies_tree.contains_key(u32_to_ivec(id)).unwrap()
+            && !movie_404_tree.contains_key(u32_to_ivec(id)).unwrap()
+        {
+            ids.push(id);
+        }
+    }
+
+    info!("Movies to be gotten = {}", ids.len());
+
+    let mut handers = vec![];
+    let semaphore = Arc::new(Semaphore::new(100));
+
+    let client = reqwest::Client::builder()
+        .timeout(Duration::from_secs(10))
+        .build()
+        .unwrap();
+
+    for id in ids {
+        let movie_404_tree = movie_404_tree.clone();
+        let permit = semaphore.clone().acquire_owned().await.unwrap();
+        let client = client.clone();
+        let movies_tree = movies_tree.clone();
+
+        let h = tokio::spawn(async move {
+            let url = format!("{site}/{id}");
+            let mut response = client.get(&url).send().await;
+            let mut cnt = 0;
+            while response.is_err() {
+                error!("{:?}", response);
+                response = client.get(&url).send().await;
+                cnt += 1;
+                if cnt >= 2 {
+                    break;
+                }
+            }
+
+            match response {
+                Ok(r) => {
+                    if r.status().is_success() {
+                        let content = r.text().await.unwrap();
+                        let movie = Movie::from(content.as_ref());
+                        let encoded = bincode::encode_to_vec(&movie, standard()).unwrap();
+                        movies_tree.insert(u32_to_ivec(id), encoded).unwrap();
+                        if id % 100 == 0 {
+                            info!("finished {}", &id);
+                        }
+                    } else if r.status() == StatusCode::NOT_FOUND {
+                        error!("{} 404 not found", id);
+                        movie_404_tree.insert(u32_to_ivec(id), &[]).unwrap();
+                    } else {
+                        error!("{:?}", r);
+                    }
+                }
+                Err(e) => error!("{:?}", e),
+            }
+            drop(permit);
+        });
+
+        handers.push(h);
+    }
+
+    for h in handers {
+        h.await.unwrap();
+    }
+
+    // download cover
+    let dir = PathBuf::from(&CONFIG.movie_cover_path);
+    if !dir.exists() {
+        std::fs::create_dir_all(&dir).unwrap();
+    }
+
+    let mut ids = vec![];
+    let movie_covers_tree = db.open_tree("movie_covers").unwrap();
+    for id in min_id..=max_id {
+        if !movie_covers_tree.contains_key(u32_to_ivec(id)).unwrap()
+            && !movie_404_tree.contains_key(u32_to_ivec(id)).unwrap()
+        {
+            ids.push(id);
+        }
+    }
+
+    info!("ids.len = {}", ids.len());
+
+    let mut handers = vec![];
+    for id in ids {
+        let movies_tree = movies_tree.clone();
+        let movie_covers_tree = movie_covers_tree.clone();
+        let permit = semaphore.clone().acquire_owned().await.unwrap();
+        let h = tokio::spawn(async move {
+            info!(id);
+            if let Some(v) = movies_tree.get(u32_to_ivec(id)).unwrap() {
+                let (movie, _): (Movie, usize) =
+                    bincode::decode_from_slice(&v, standard()).unwrap();
+                if let Some(cover) = movie.cover {
+                    let url = format!("https://neodb.social{cover}");
+                    let ext = url.rsplit_once('.').unwrap().1;
+                    let mut response = CLIENT.get(&url).send().await;
+
+                    let mut cnt = 0;
+                    while response.is_err() {
+                        error!("{:?}", response);
+                        response = CLIENT.get(&url).send().await;
+                        cnt += 1;
+                        if cnt >= 2 {
+                            break;
+                        }
+                    }
+
+                    if let Ok(r) = response {
+                        if r.status().is_success() {
+                            let fpath = format!("{}/{}.{}", &CONFIG.movie_cover_path, id, ext);
+                            let content = r.bytes().await.unwrap();
+                            tokio::fs::write(fpath, content).await.unwrap();
+                            movie_covers_tree.insert(u32_to_ivec(id), &[]).unwrap();
+                            if id % 100 == 0 {
+                                info!("finished {}", &id);
+                            }
+                        } else {
+                            error!("{:?}", r);
+                        }
+                    } else {
+                        error!("{:?}", response);
+                    }
+                }
+            }
+            drop(permit);
+        });
+        handers.push(h);
+    }
+
+    for h in handers {
+        h.await.unwrap();
+    }
+
+    // books
     let books_tree = db.open_tree("books").unwrap();
     let db_max_id = if let Some((k, _)) = books_tree.last().unwrap() {
         ivec_to_u32(&k)
@@ -26,8 +184,8 @@ async fn main() {
     };
     info!(%db_max_id);
 
-    let min_id = CONFIG.min_id.unwrap_or(db_max_id);
-    let max_id = CONFIG.max_id;
+    let min_id = CONFIG.book_min_id.unwrap_or(db_max_id);
+    let max_id = CONFIG.book_max_id;
     let site = "https://neodb.social/books";
 
     info!(%min_id);
--- src/extract.rs
+++ src/extract.rs
@@ -3,23 +3,34 @@ use once_cell::sync::Lazy;
 use scraper::{Html, Selector};
 
 const TITLE_STR: &str = r#"title"#;
-const COVER_STR: &str = r#"a[class="entity-detail__img-origin"]"#;
 const H5_STR: &str = r#"h5"#;
 const A_STR: &str = r#"a"#;
-const DETAILS_STR: &str = r#"div[class="entity-detail__fields"]"#;
 const DIV_STR: &str = r#"div"#;
 const SPAN_STR: &str = r#"span"#;
+
+const COVER_STR: &str = r#"a[class="entity-detail__img-origin"]"#;
+const DETAILS_STR: &str = r#"div[class="entity-detail__fields"]"#;
 const TAG_STR: &str = r#"span[class="tag-collection__tag"]"#;
 const DESC_STR: &str = r#"p[class="entity-desc__content"]"#;
+
+const DIRECTOR_STR: &str = r#"span[class="director"]"#;
+const PLAYWRIGHT_STR: &str = r#"span[class="playwright"]"#;
+const ACTOR_STR: &str = r#"span[class="actor"]"#;
+
 static TITLE: Lazy<Selector> = Lazy::new(|| Selector::parse(TITLE_STR).unwrap());
-static COVER: Lazy<Selector> = Lazy::new(|| Selector::parse(COVER_STR).unwrap());
 static H5: Lazy<Selector> = Lazy::new(|| Selector::parse(H5_STR).unwrap());
 static A: Lazy<Selector> = Lazy::new(|| Selector::parse(A_STR).unwrap());
-static DETAILS: Lazy<Selector> = Lazy::new(|| Selector::parse(DETAILS_STR).unwrap());
 static DIV: Lazy<Selector> = Lazy::new(|| Selector::parse(DIV_STR).unwrap());
 static SPAN: Lazy<Selector> = Lazy::new(|| Selector::parse(SPAN_STR).unwrap());
+
+static COVER: Lazy<Selector> = Lazy::new(|| Selector::parse(COVER_STR).unwrap());
+static DETAILS: Lazy<Selector> = Lazy::new(|| Selector::parse(DETAILS_STR).unwrap());
 static TAG: Lazy<Selector> = Lazy::new(|| Selector::parse(TAG_STR).unwrap());
 static DESC: Lazy<Selector> = Lazy::new(|| Selector::parse(DESC_STR).unwrap());
+
+static DIRECTOR: Lazy<Selector> = Lazy::new(|| Selector::parse(DIRECTOR_STR).unwrap());
+static PLAYWRIGHT: Lazy<Selector> = Lazy::new(|| Selector::parse(PLAYWRIGHT_STR).unwrap());
+static ACTOR: Lazy<Selector> = Lazy::new(|| Selector::parse(ACTOR_STR).unwrap());
 
 #[derive(Debug, Encode, Decode)]
 pub struct Book {
@@ -166,6 +177,171 @@ impl From<&str> for Book {
             tags,
             description,
             content,
+        }
+    }
+}
+
+#[derive(Debug, Encode, Decode)]
+pub struct Movie {
+    title: String,
+    pub cover: Option<String>,
+    source: Option<String>,
+    imdb: Option<String>,
+    directors: Vec<String>,
+    writers: Vec<String>,
+    stars: Vec<String>,
+    genres: Vec<String>,
+    countries: Vec<String>,
+    languages: Vec<String>,
+    runtime: Option<String>,
+    season: Option<String>,
+    episode: Option<String>,
+    episode_runtime: Option<String>,
+    release_dates: Vec<String>,
+    alias: Vec<String>,
+    tags: Vec<String>,
+    description: Option<String>,
+}
+
+impl From<&str> for Movie {
+    fn from(html: &str) -> Self {
+        let fragment = Html::parse_fragment(html);
+
+        let title = fragment
+            .select(&TITLE)
+            .next()
+            .unwrap()
+            .inner_html()
+            .rsplit_once("| ")
+            .unwrap()
+            .1
+            .to_owned();
+
+        let cover = fragment
+            .select(&COVER)
+            .next()
+            .unwrap()
+            .value()
+            .attr("href")
+            .map(|s| s.to_owned());
+
+        let source = fragment
+            .select(&H5)
+            .next()
+            .unwrap()
+            .select(&A)
+            .next()
+            .unwrap()
+            .value()
+            .attr("href")
+            .map(|s| s.to_owned());
+
+        // details
+        let mut details = fragment.select(&DETAILS);
+
+        // first parts
+        let mut div_ele = details.next().unwrap().select(&DIV).skip(1);
+
+        let imdb = div_ele
+            .next()
+            .map(|ele| ele.select(&A).next().unwrap().inner_html());
+
+        let directors = fragment
+            .select(&DIRECTOR)
+            .map(|ele| ele.inner_html())
+            .collect();
+
+        let writers = fragment
+            .select(&PLAYWRIGHT)
+            .map(|ele| ele.inner_html())
+            .collect();
+
+        let stars = fragment
+            .select(&ACTOR)
+            .map(|ele| ele.inner_html())
+            .collect();
+
+        let genres = div_ele
+            .nth(3)
+            .unwrap()
+            .select(&SPAN)
+            .map(|ele| ele.inner_html())
+            .collect();
+
+        let countries = div_ele
+            .next()
+            .unwrap()
+            .select(&SPAN)
+            .map(|ele| ele.inner_html())
+            .collect();
+
+        let languages = div_ele
+            .next()
+            .unwrap()
+            .select(&SPAN)
+            .map(|ele| ele.inner_html())
+            .collect();
+
+        // second parts
+        let mut div_ele = details.next().unwrap().select(&DIV);
+
+        let runtime = div_ele
+            .next()
+            .map(|ele| ele.inner_html().trim_start_matches("片长：").to_owned());
+
+        let season = div_ele
+            .next()
+            .map(|ele| ele.inner_html().trim_start_matches("季数：").to_owned());
+
+        let episode = div_ele
+            .next()
+            .map(|ele| ele.inner_html().trim_start_matches("集数：").to_owned());
+
+        let episode_runtime = div_ele
+            .next()
+            .map(|ele| ele.inner_html().trim_start_matches("单集长度：").to_owned());
+
+        let release_dates = div_ele
+            .next()
+            .unwrap()
+            .select(&SPAN)
+            .map(|ele| ele.inner_html())
+            .collect();
+
+        let alias = div_ele
+            .next()
+            .unwrap()
+            .select(&SPAN)
+            .map(|ele| ele.inner_html())
+            .collect();
+
+        let tags = fragment
+            .select(&TAG)
+            .map(|ele| ele.select(&A).next().unwrap().inner_html())
+            .collect();
+
+        let mut div_ele = fragment.select(&DESC);
+        let description = div_ele.next().map(|ele| ele.inner_html().trim().to_owned());
+
+        Movie {
+            title,
+            cover,
+            source,
+            imdb,
+            directors,
+            writers,
+            stars,
+            genres,
+            countries,
+            languages,
+            runtime,
+            season,
+            episode,
+            episode_runtime,
+            release_dates,
+            alias,
+            tags,
+            description,
         }
     }
 }
